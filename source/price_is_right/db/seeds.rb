19c8bae05b4e5791ef3fe29582a486a8022c0c4d
Marisa Ramon & Nicholas Toulouse <pair+mpieces.nicoto@devbootcamp.com>
2015-06-05 11:01:43 -0700

    seed dot rb filled

--- source/price_is_right/db/seeds.rb
+++ source/price_is_right/db/seeds.rb
@@ -1,7 +1,39 @@
-canoe_kit = { name: "Build Your Own Canoe Kit", price: 2_195 }
-marisa = { name: "Marisa" }
-marisas_bid = { bid: 1500, item_id: 1, contestant_id: 1 }
-
+canoe_kit = { name: "19 foot build Your Own Canoe Kit", price: 2_195 }
 Item.create(canoe_kit)
-Contestant.create(marisa)
-Bid.create(marisas_bid)
+
+inflatable_ball_pit = { name: "Magic Castle Commercial inflatable ball pit bounce house", price: 2_340 }
+Item.create(inflatable_ball_pit)
+
+hat = { name: "Go Warriors Hat with Puff Ball", price: 24 }
+Item.create(hat)
+
+skates = { name: "Roller Derby AERIO Q-60 women's inline skates", price: 60 }
+Item.create(skates)
+
+iphone6= { name: "unlocked iPhone6 Plus 5.5 inch display 64GB storage", price: 849 }
+Item.create()
+
+massager = { name: "body back buddy classic", price: 35 }
+Item.create(massager)
+
+bike = { name: "Hourly rental for a tandem bike in Venice Beach, LA", price: 13 }
+Item.create(bike)
+
+selfie_stick = { name: "A selfie stick accessory for cellphone camera", price: 20 }
+Item.create(selfie_stick)
+
+robot_dog = { name: "Hasbro Intelligent Robotic Dog", price: 69 }
+Item.create(robot_dog)
+
+drone = { name: "QuadroCopter Drone with HD video camera", price: 1100 }
+Item.create(drone)
+
+motorcycle = { name: "Above-average Harley Davidson Sportster Custom", price: 25_000 }
+Item.create(motorcycle)
+
+cruise = { name: "Princess 11 day Alaskan cruise from San Francisco (balcony suite)", price: 2_025 }
+Item.create(cruise)
+
+cruise = { name: "Dollface chihuahua or pug puppy", price: 2_000 }
+Item.create(cruise)
+
